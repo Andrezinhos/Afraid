0d77e65886976f548a13dc454b234d40fd72afb7
Ryuukomo <98666355+Ryuukomo@users.noreply.github.com>
2025-09-09 17:13:56 -0300

    adicionei um código sem o andré saber

---
+++ Assets/Script/GuardarDados.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GuardarDados : MonoBehaviour
+{
+    public static int Cristal = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GuardarDados.Cristal = 0;
+    }
+}
