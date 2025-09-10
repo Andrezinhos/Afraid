38285a08ebf0ac07b02823e3cfb17ccbaf938d1a
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-10 17:18:07 -0300

    iluminação cenario

---
+++ Assets/Script/Input teste.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class Inputteste : MonoBehaviour
+{
+    Rigidbody2D rigib;
+
+    float horizontal;
+    float vertical;
+
+    public float velo = 5;
+
+    bool estaDialogo = false;
+
+    void Start()
+    {
+        rigib = transform.GetComponent<Rigidbody2D>();
+        rigib.rotation = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            ProcessaInputs();
+    }
+
+    void FixedUpdate()
+    {
+        MoveJogador();
+        //RotacionarMovimento();
+        //RotacionaMouse();
+    }
+
+    void ProcessaInputs()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void MoveJogador()
+    {
+        Vector2 move = new Vector2(horizontal, vertical);
+        if (move.magnitude > 1)
+        {
+            move = move.normalized;
+        }
+        move = move * velo;
+
+        rigib.linearVelocity = move;
+    }
+
+    //void RotacionarMovimento()
+    //{
+    //    //return early
+    //    if (vertical == 0 && horizontal == 0)
+    //    {
+    //        return;
+    //    }
+
+    //    float angulo = Mathf.Atan2(vertical, horizontal) * Mathf.Rad2Deg;
+    //    rigib.rotation = angulo;
+    //}
+}
