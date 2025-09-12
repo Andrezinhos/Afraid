adc1373ed3d47f8267be222006763277822ffa37
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-12 13:45:14 -0300

    modificação de luz

--- Assets/Script/Input teste.cs
+++ Assets/Script/Input teste.cs
@@ -26,8 +26,6 @@ public class Inputteste : MonoBehaviour
     void FixedUpdate()
     {
         MoveJogador();
-        RotacionarMovimento();
-        //RotacionaMouse();
     }
 
     void ProcessaInputs()
@@ -46,17 +44,5 @@ public class Inputteste : MonoBehaviour
         move = move * velo;
 
         rigib.linearVelocity = move;
-    }
-
-    void RotacionarMovimento()
-    {
-        //return early
-        if (vertical == 0 && horizontal == 0)
-        {
-            return;
-        }
-
-        float angulo = Mathf.Atan2(vertical, horizontal) * Mathf.Rad2Deg;
-        rigib.rotation = angulo;
     }
 }
