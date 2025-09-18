3240449ab41ad36921703ece76192a3bb7588960
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-18 16:18:56 -0300

    feito modificações na cena

--- Assets/Script/MoviLetra.cs
+++ Assets/Script/MoviLetra.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class MoviLetra : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // A altura m xima que a plataforma vai subir e descer
+    public float alturaLimite = 5f;
+
+    // A velocidade da plataforma
+    public float velocidade = 2f;
+
+    // A posi  o inicial da plataforma
+    private Vector3 posicaoInicial;
+
     void Start()
     {
-        
+        posicaoInicial = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float novaPosicaoY = posicaoInicial.y + Mathf.Sin(Time.time * velocidade) * alturaLimite;
+
+        transform.position = new Vector3(posicaoInicial.x, novaPosicaoY, posicaoInicial.z);
     }
 }
